4b3caa7b44ae87f95fd028636d13d04eeb0fac4e
dongHun <actgong@gmail.com>
2023-08-03 10:02:23 +0900

    [Softeer] level 2 지도 자동 구축

---
+++ src/지도_자동_구축.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class 지도_자동_구축 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        int iter = 2;
+        for(int i = 1; i <= n; i++){
+            iter = iter + (iter - 1);
+        }
+
+        System.out.println(iter * iter);
+    }
+}
